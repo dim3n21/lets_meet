acf128ccea02bd56e732ebfa23c09aacac62f014
Dmitry Andrianov <dima.andrianov@gmail.com>
2019-08-29 15:21:37 -0700

    Add Route configuration.

--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Menu, Container, Button } from 'semantic-ui-react';
+import { NavLink, Link } from 'react-router-dom';
 
 class NavBar extends Component {
       render() {
@@ -7,13 +8,14 @@ class NavBar extends Component {
                   <div>
                         <Menu inverted fixed="top">
                               <Container>
-                                    <Menu.Item header>
+                                    <Menu.Item as={NavLink} exact to="/" header>
                                           <img src="assets/logo.png" alt="logo" />
                                           Let's Meet
                                     </Menu.Item>
-                                    <Menu.Item name="Events" />
+                                    <Menu.Item as={NavLink} to='/events' name="Events" />
+                                    <Menu.Item as={NavLink} to='/people' name="People" />
                                     <Menu.Item>
-                                          <Button floated="right" inverted content="Create Event" />
+                                          <Button as={Link} to="/createEvent" floated="right" inverted content="Create Event" />
                                     </Menu.Item>
                                     <Menu.Item position="right">
                                           <Button basic inverted content="Login" />
