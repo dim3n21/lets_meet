0f2190f27adffcc4167a02964c063e435fcf4e38
Dmitry Andrianov <dima.andrianov@gmail.com>
2019-08-30 10:27:10 -0700

    Add internal navigation for our application.

--- src/features/user/Settings/SettingsDashboard.jsx
+++ src/features/user/Settings/SettingsDashboard.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
 
+import { Grid } from 'semantic-ui-react';
+import { Route, Redirect, Switch } from 'react-router-dom';
+
+import SettingsNav from '../Settings/SettingsNav';
+import BasicPage from './BasicPage';
+import AboutPage from './AboutPage';
+import PhotosPage from './PhotosPage';
+import AccountPage from './AccountPage';
+
+
 const SettingsDashboard = () => {
       return (
-            <div>
-                  <h1>Settings dashboard</h1>
-            </div>
+            <Grid>
+                  <Grid.Column width="12">
+                        <Switch>
+                              <Redirect exact from='/settings' to='/settings/basic' />
+                              <Route path='/settings/basic' component={BasicPage} />
+                              <Route path='/settings/about' component={AboutPage} />
+                              <Route path='/settings/photos' component={PhotosPage} />
+                              <Route path='/settings/account' component={AccountPage} />
+                        </Switch>
+                  </Grid.Column>
+                  <Grid.Column width="4">
+                        <h1><SettingsNav /></h1>
+                  </Grid.Column>
+            </Grid>
       );
 };
 
