e0cb4c462c975c578f35e4a0924eee45789979ff
Dmitry Andrianov <dima.andrianov@gmail.com>
2019-09-03 15:53:49 -0700

    Create Redux store. Creade event reducer. Create root reducer, to combine all reducers in future. Create event constants to simplify the code. Add event actions. Implement redux to EventDashboard components. Add devToolEnhancer from reduz devtool extension for using devtool extension in the project.

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -1,66 +1,19 @@
 import React, { Component } from 'react';
 import cuid from 'cuid';
+import { connect } from 'react-redux';
 
 import { Grid, Button } from 'semantic-ui-react';
 import EventList from '../EventList/EventList';
 import EventForm from '../EventForm/EventForm';
 
-const eventsDashBoard = [
-  {
-    id: '1',
-    title: 'Trip to Tower of London',
-    date: '2018-03-27',
-    category: 'culture',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
-    city: 'London, UK',
-    venue: "Tower of London, St Katharine's & Wapping, London",
-    hostedBy: 'Bob',
-    hostPhotoURL: 'https://randomuser.me/api/portraits/men/20.jpg',
-    attendees: [
-      {
-        id: 'a',
-        name: 'Bob',
-        photoURL: 'https://randomuser.me/api/portraits/men/20.jpg'
-      },
-      {
-        id: 'b',
-        name: 'Tom',
-        photoURL: 'https://randomuser.me/api/portraits/men/22.jpg'
-      }
-    ]
-  },
-  {
-    id: '2',
-    title: 'Trip to Punch and Judy Pub',
-    date: '2018-03-28',
-    category: 'drinks',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
-    city: 'London, UK',
-    venue: 'Punch & Judy, Henrietta Street, London, UK',
-    hostedBy: 'Tom',
-    hostPhotoURL: 'https://randomuser.me/api/portraits/men/22.jpg',
-    attendees: [
-      {
-        id: 'b',
-        name: 'Tom',
-        photoURL: 'https://randomuser.me/api/portraits/men/22.jpg'
-      },
-      {
-        id: 'a',
-        name: 'Bob',
-        photoURL: 'https://randomuser.me/api/portraits/men/20.jpg'
-      }
-    ]
-  }
-]
+const mapStateToProps = (state) => ({
+  events: state.events
+})
 
 
 class EventDashboard extends Component {
 
   state = {
-    events: eventsDashBoard,
     isOpen: false,
     selectedEvent: null
   }
@@ -130,7 +83,8 @@ class EventDashboard extends Component {
   
 
   render() {  
-  const {events, isOpen, selectedEvent} = this.state;
+  const {isOpen, selectedEvent} = this.state;
+  const {events} = this.props;
     return (
         <div>
               <Grid>
@@ -159,4 +113,4 @@ class EventDashboard extends Component {
   }
 }
 
-export default EventDashboard;
+export default connect(mapStateToProps,)(EventDashboard);
