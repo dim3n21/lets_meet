561bcf372b325e297dbe626354b7fcc436641261
Dmitry Andrianov <dima.andrianov@gmail.com>
2019-09-04 12:53:04 -0700

    Connect the event detaied page to the store.

--- src/features/event/EventDetailed/EventDetailedPage.jsx
+++ src/features/event/EventDetailed/EventDetailedPage.jsx
@@ -1,37 +1,27 @@
 import React from 'react';
 import { Grid } from 'semantic-ui-react';
+import { connect } from 'react-redux';
 
 import EventDetailedHeader from './EventDetailedHeader';
 import EventDetailedInfo from './EventDetailedInfo';
 import EventDetailedChat from './EventDetailedChat';
 import EventDetailedSidebar from './EventDetailedSidebar';
 
-const event = {
-      id: '1',
-      title: 'Trip to Tower of London',
-      date: '2018-03-27',
-      category: 'culture',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
-      city: 'London, UK',
-      venue: "Tower of London, St Katharine's & Wapping, London",
-      hostedBy: 'Bob',
-      hostPhotoURL: 'https://randomuser.me/api/portraits/men/20.jpg',
-      attendees: [
-        {
-          id: 'a',
-          name: 'Bob',
-          photoURL: 'https://randomuser.me/api/portraits/men/20.jpg'
-        },
-        {
-          id: 'b',
-          name: 'Tom',
-          photoURL: 'https://randomuser.me/api/portraits/men/22.jpg'
-        }
-      ]
-    }
+const mapStateToProps = (state, ownProps) => {
+      const eventId = ownProps.match.params.id;
 
-const EventDetailed = () => {
+      let event = {};
+
+      if (eventId && state.events.length > 0) {
+            event = state.events.filter( event => event.id === eventId)[0];
+      }
+
+      return {
+            event
+      }
+}
+
+const EventDetailed = ({event}) => {
       return (
             <Grid>
                   <Grid.Column width={10}>
@@ -46,4 +36,4 @@ const EventDetailed = () => {
       );
 };
 
-export default EventDetailed;
+export default connect(mapStateToProps)(EventDetailed);
