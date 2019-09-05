998c256fc59ad82a54f9328751ab8d39608f82b9
Dmitry Andrianov <dima.andrianov@gmail.com>
2019-09-05 14:51:54 -0700

    Set up Places Autocomplete.

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -11,6 +11,8 @@ import UserDetailedPage from '../../features/user/UserDetailed/UserDetailedPage'
 import SettingsDashboard from '../../features/user/Settings/SettingsDashboard';
 import EventForm from '../../features/event/EventForm/EventForm';
 
+import TestComponent from '../../features/test-area/TestComponents';
+
 class App extends Component {
   render () {
     return (
@@ -21,6 +23,7 @@ class App extends Component {
               render={ () => (
                 <Fragment>
                   <NavBar />
+                  <TestComponent />
                   <Container className="main">
                     <Switch key={this.props.location.key}>
                       <Route exact path='/events' component={EventDashboard} />
@@ -29,6 +32,7 @@ class App extends Component {
                       <Route path='/profile/:id' component={UserDetailedPage} />
                       <Route path='/settings' component={SettingsDashboard} />
                       <Route path={['/createEvent', '/manage/:id']} component={EventForm} />
+                      <Route path='/test' component = {TestComponent} />
                     </Switch>
                   </Container>
               </Fragment>
---
+++ src/features/test-area/TestComponents.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import TestPlaceInput from './TestPlaceInput';
+
+class TestComponents extends Component {
+      render() {
+            return (
+                  <div>
+                  < TestPlaceInput />
+                  </div>
+            );
+      }
+}
+
+export default TestComponents;
