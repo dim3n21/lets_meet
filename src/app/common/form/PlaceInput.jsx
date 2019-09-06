09da1806aa0af16da838162e7c359ddfe748db46
Dmitry Andrianov <dima.andrianov@gmail.com>
2019-09-06 14:51:01 -0700

    Narrow the venue results to the selected city.

--- src/app/common/form/PlaceInput.jsx
+++ src/app/common/form/PlaceInput.jsx
@@ -7,6 +7,7 @@ const PlaceInput = ({
             width,
             options,
             placeholder,
+            onSelect,
             meta: {touched, error}
       }) => {
       
@@ -15,6 +16,7 @@ const PlaceInput = ({
                   value={value}
                   onChange={onChange}
                   searchOptions={options}
+                  onSelect={onSelect}
             >
                   {({getInputProps, suggestions, getSuggestionItemProps, loading}) => (
                         <Form.Field error={touched && !!error}>
