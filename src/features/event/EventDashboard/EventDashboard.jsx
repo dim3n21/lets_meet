685db1a796db965586f4c90eb428e2b2b702af82
Dmitry Andrianov <dima.andrianov@gmail.com>
2019-09-10 14:35:57 -0700

    Add a Loading Component.

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -5,9 +5,11 @@ import { Grid } from 'semantic-ui-react';
 import EventList from '../EventList/EventList';
 
 import { createEvent, deleteEvent, updateEvent } from '../eventActions';
+import LoadingComponent from '../../../app/layout/LoadingComponent';
 
 const mapStateToProps = (state) => ({
-  events: state.events
+  events: state.events,
+  loading: state.async.loading
 })
 
 const mapDispatchToProps = {
@@ -22,11 +24,9 @@ class EventDashboard extends Component {
     this.props.deleteEvent(id);
   };
 
-  
-  
-
   render() {  
-  const {events} = this.props;
+  const {events, loading} = this.props;
+    if (loading) return <LoadingComponent />
     return (
         <div>
               <Grid>
