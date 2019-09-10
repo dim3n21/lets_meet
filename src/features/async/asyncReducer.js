faeb0ee0ae2f572562e5fa71c95630cc3ad8803e
Dmitry Andrianov <dima.andrianov@gmail.com>
2019-09-10 12:19:44 -0700

    Create a boilerplate for async reducer.

---
+++ src/features/async/asyncReducer.js
@@ -0,0 +1,34 @@
+import { createReducer } from "../../app/common/util/reducerUtils"
+import { ASYNC_ACTION_START, ASYNC_ACTION_FINISH, ASYNC_ACTION_ERROR } from "./asyncConstants"
+
+const initialState = {
+      loading: false
+}
+
+const asyncActionStart = (state) => {
+      return {
+            ...state, 
+            loading: true
+      }
+}
+
+const asyncActionFinished = (state) => {
+      return {
+            ...state, 
+            loading: false
+      }
+}
+
+const asyncActionError = (state) => {
+      return {
+            ...state, 
+            loading: false
+      }
+}
+
+export default createReducer(initialState, {
+      [ASYNC_ACTION_START]: asyncActionStart,
+      [ASYNC_ACTION_FINISH]: asyncActionFinished,
+      [ASYNC_ACTION_ERROR]: asyncActionError
+
+})
