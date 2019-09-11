325610b42eee2698fef76542339432c4afc9fee2
Dmitry Andrianov <dima.andrianov@gmail.com>
2019-09-11 15:38:07 -0700

    Create and attache the firebase project to our app.

---
+++ src/app/config/firebase.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/database';
+import 'firebase/auth';
+import 'firebase/storage';
+
+import { firebaseAPI } from '../security/google';
+
+const firebaseConfig = {
+      apiKey: firebaseAPI,
+      authDomain: "lets-meet-6cae4.firebaseapp.com",
+      databaseURL: "https://lets-meet-6cae4.firebaseio.com",
+      projectId: "lets-meet-6cae4",
+      storageBucket: "",
+      messagingSenderId: "621508718903",
+      appId: "1:621508718903:web:3fc1839267e8acd4882ab2"
+    };
+
+    firebase.initializeApp(firebaseConfig);
+    firebase.firestore();
+
+    export default firebase;
