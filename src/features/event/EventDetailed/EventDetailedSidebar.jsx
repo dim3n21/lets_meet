f7c6ae6478f0f160b007acbb2ff28b2ac69c5c8f
Dmitry Andrianov <dima.andrianov@gmail.com>
2019-10-03 16:43:34 -0700

    Fix errors with Time. Convert objects to arrays.

--- src/features/event/EventDetailed/EventDetailedSidebar.jsx
+++ src/features/event/EventDetailed/EventDetailedSidebar.jsx
@@ -3,46 +3,46 @@ import { Fragment } from 'react';
 
 import { Segment, Item, Label, List } from 'semantic-ui-react';
 
-const EventDetailedSidebar = ({attendees}) => {
+const EventDetailedSidebar = ({ attendees }) => {
 
       const isHost = false;
 
       return (
-                <Fragment>
+            <Fragment>
                   <Segment
-                    textAlign='center'
-                    style={{ border: 'none' }}
-                    attached='top'
-                    secondary
-                    inverted
-                    color='green'
+                        textAlign='center'
+                        style={{ border: 'none' }}
+                        attached='top'
+                        secondary
+                        inverted
+                        color='green'
                   >
-                    {attendees && attendees.length}{' '}
-        {attendees && attendees.length === 1 ? 'Person' : 'People'} going
+                        {attendees && attendees.length}{' '}
+                        {attendees && attendees.length === 1 ? 'Person' : 'People'} going
                   </Segment>
                   <Segment attached>
                         <List relaxed devided="true">
                               {attendees && attendees.map(attendee => (
                                     <Item.Group divided key={attendee.id}>
-                                    <Item style={{ position: 'relative' }}>
-                                          {isHost && <Label
-                                          style={{ position: 'absolute' }}
-                                          color='orange'
-                                          ribbon='right'
-                                          >
-                                                Host
-                                          </Label> }
-                                          <Item.Image size='tiny' src={attendee.photoURL} />
-                                          <Item.Content verticalAlign='middle'>
-                                                <Item.Header as='h3'>{attendee.name}</Item.Header>
-                                          </Item.Content>
-                                    </Item>
-                              </Item.Group>
+                                          <Item style={{ position: 'relative' }}>
+                                                {isHost && <Label
+                                                      style={{ position: 'absolute' }}
+                                                      color='orange'
+                                                      ribbon='right'
+                                                >
+                                                      Host
+                                          </Label>}
+                                                <Item.Image size='tiny' src={attendee.photoURL} />
+                                                <Item.Content verticalAlign='middle'>
+                                                      <Item.Header as='h3'>{attendee.displayName}</Item.Header>
+                                                </Item.Content>
+                                          </Item>
+                                    </Item.Group>
                               ))}
-                              
+
                         </List>
                   </Segment>
-                </Fragment>
+            </Fragment>
       );
 };
 
