9f736f22fa47293dfff1db6bc136375f9074c266
Dmitry Andrianov <dima.andrianov@gmail.com>
2019-10-03 16:30:23 -0700

    Getting data from Firestore.

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -1,189 +1,209 @@
 /* global google */
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { reduxForm, Field } from 'redux-form';
-import { composeValidators, combineValidators, isRequired, hasLengthGreaterThan } from 'revalidate';
-import { Segment, Form, Button, Grid, Header } from 'semantic-ui-react'
-import cuid from 'cuid';
-import {geocodeByAddress, getLatLng} from 'react-places-autocomplete';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { reduxForm, Field } from "redux-form";
+import {
+  composeValidators,
+  combineValidators,
+  isRequired,
+  hasLengthGreaterThan
+} from "revalidate";
+import { Segment, Form, Button, Grid, Header } from "semantic-ui-react";
+import { geocodeByAddress, getLatLng } from "react-places-autocomplete";
 
-import {updateEvent, createEvent} from '../eventActions';
-import TextInput from '../../../app/common/form/TextInput';
-import TextArea from '../../../app/common/form/TextArea';
-import SelectInput from '../../../app/common/form/SelectInput';
-import DateInput from '../../../app/common/form/DateInput';
-import PlaceInput from '../../../app/common/form/PlaceInput';
-
-
+import { updateEvent, createEvent } from "../eventActions";
+import TextInput from "../../../app/common/form/TextInput";
+import TextArea from "../../../app/common/form/TextArea";
+import SelectInput from "../../../app/common/form/SelectInput";
+import DateInput from "../../../app/common/form/DateInput";
+import PlaceInput from "../../../app/common/form/PlaceInput";
 
 const mapStateToProps = (state, ownProps) => {
-      const eventId = ownProps.match.params.id;
+  const eventId = ownProps.match.params.id;
 
-      let event = {};
+  let event = {};
 
-      if (eventId && state.events.length > 0) {
-            event = state.events.filter( event => event.id === eventId)[0];
-      }
+  if (eventId && state.events.length > 0) {
+    event = state.events.filter(event => event.id === eventId)[0];
+  }
 
-      return {
-            initialValues: event
-      }
-}
+  return {
+    initialValues: event
+  };
+};
 
 const mapDispatchToProps = {
-      updateEvent,
-      createEvent
-}
+  updateEvent,
+  createEvent
+};
 
 const validate = combineValidators({
-      title: isRequired({message: 'The event title is required'}),
-      category: isRequired({message: 'The category title is required'}),
-      description: composeValidators(
-            isRequired({message: 'Please enter the description'}),
-            hasLengthGreaterThan(4)({message: 'Description needs to be at least 5 characters'})
-      )(),
-      city: isRequired('city'),
-      venue: isRequired('venue'),
-      date: isRequired('date')
-})
+  title: isRequired({ message: "The event title is required" }),
+  category: isRequired({ message: "The category title is required" }),
+  description: composeValidators(
+    isRequired({ message: "Please enter the description" }),
+    hasLengthGreaterThan(4)({
+      message: "Description needs to be at least 5 characters"
+    })
+  )(),
+  city: isRequired("city"),
+  venue: isRequired("venue"),
+  date: isRequired("date")
+});
 
 const category = [
-    {key: 'drinks', text: 'Drinks', value: 'drinks'},
-    {key: 'culture', text: 'Culture', value: 'culture'},
-    {key: 'film', text: 'Film', value: 'film'},
-    {key: 'food', text: 'Food', value: 'food'},
-    {key: 'music', text: 'Music', value: 'music'},
-    {key: 'travel', text: 'Travel', value: 'travel'},
+  { key: "drinks", text: "Drinks", value: "drinks" },
+  { key: "culture", text: "Culture", value: "culture" },
+  { key: "film", text: "Film", value: "film" },
+  { key: "food", text: "Food", value: "food" },
+  { key: "music", text: "Music", value: "music" },
+  { key: "travel", text: "Travel", value: "travel" }
 ];
 
 class EventForm extends Component {
+  state = {
+    cityLatLng: {},
+    venueLatLng: {}
+  };
 
-      state = {
-           cityLatLng: {},
-           venueLatLng: {}
-          }
+  onFormSubmit = async values => {
+    values.venueLatLng = this.state.venueLatLng;
+    try {
+      if (this.props.initialValues.id) {
+        this.props.updateEvent(values);
+        this.props.history.push(`/events/${this.props.initialValues.id}`);
+      } else {
+        let createdEvent = await this.props.createEvent(values);
+        this.props.history.push(`/events/${createdEvent.id}`);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-      onFormSubmit = async values => {
-            values.venueLatLng = this.state.venueLatLng;
-            try {
-                  if (this.props.initialValues.id) {
-                        this.props.updateEvent(values);
-                        this.props.history.push(`/events/${this.props.initialValues.id}`);
-                  } else {
-                        let createdEvent = await this.props.createEvent(values);
-                        this.props.history.push(`/events/${createdEvent.id}`);
-                  }     
-            } catch (error) {
-                  console.log(error)
-            }
-           
-      }
+  handleCitySelect = selectedCity => {
+    geocodeByAddress(selectedCity)
+      .then(results => getLatLng(results[0]))
+      .then(latlng => {
+        this.setState({
+          cityLatLng: latlng
+        });
+      })
+      .then(() => {
+        this.props.change("city", selectedCity);
+      });
+  };
 
-      handleCitySelect = selectedCity => {
-            geocodeByAddress(selectedCity)
-                  .then(results => getLatLng(results[0]))
-                  .then(latlng => {
-                        this.setState({
-                              cityLatLng: latlng
-                        })
-                  })
-                  .then(() => {
-                        this.props.change('city', selectedCity)
-                  })
-      }
+  handleVenueSelect = selectedVenue => {
+    geocodeByAddress(selectedVenue)
+      .then(results => getLatLng(results[0]))
+      .then(latlng => {
+        this.setState({
+          venueLatLng: latlng
+        });
+      })
+      .then(() => {
+        this.props.change("venue", selectedVenue);
+      });
+  };
 
-      handleVenueSelect = selectedVenue => {
-            geocodeByAddress(selectedVenue)
-                  .then(results => getLatLng(results[0]))
-                  .then(latlng => {
-                        this.setState({
-                              venueLatLng: latlng
-                        })
-                  })
-                  .then(() => {
-                        this.props.change('venue', selectedVenue)
-                  })
-      }
-      
-      render() {
-            const {history, initialValues, invalid, submitting, pristine} =  this.props
-            return (
-                  <Grid>
-                        <Grid.Column width={10}>
-                              <Segment>
-                                    <Form autoComplete="off" onSubmit={this.handleFormSubmit}>
-                                          <Header sub color='green' content='Event Details' />
-                                          <Form onSubmit={this.props.handleSubmit(this.onFormSubmit)}                   autoComplete="off">
-                                                <Field
-                                                      name="title"
-                                                      component={TextInput}
-                                                      placeholder="Event Name" />
-                                                
-                                                <Field
-                                                      name="category"
-                                                      component={SelectInput}
-                                                      options={category} 
-                                                      placeholder="Event Category" />
-                                          
-                                                <Field
-                                                      name="description"
-                                                      component={TextArea}
-                                                      rows={3}
-                                                      placeholder="Event Decription" />
-                                                
-                                                <Header
-                                                      sub color='green'
-                                                      content='Event Location Details' />
-                                                
-                                                <Field
-                                                      name="city"
-                                                      component={PlaceInput}
-                                                      options={{type: ['(cities)']}}
-                                                      onSelect={this.handleCitySelect}
-                                                      placeholder="Event City" />
-                                                
-                                                <Field
-                                                      name="venue"
-                                                      component={PlaceInput}
-                                                      options={{
-                                                            location: new google.maps.LatLng(this.state.cityLatLng),
-                                                            radius: 1000,
-                                                            types: ['establishment']
-                                                      }}
-                                                      onSelect={this.handleVenueSelect}
-                                                      placeholder="Event Venue" />
-                                                
-                                                <Field
-                                                      name="date"
-                                                      component={DateInput}
-                                                      dateFormat='dd LLL yyyy h:mm a'
-                                                      showTimeSelect
-                                                      timeFormat='HH:mm'
-                                                      placeholder="Event Date" />
-                                                
-                                                <Button
-                                                      disabled={invalid || submitting || pristine}
-                                                      positive
-                                                      type="submit">
-                                                            Submit 
-                                                </Button>
-                                                
-                                                <Button
-                                                      type="button"
-                                                      onClick={
-                                                            initialValues.id
-                                                                  ? () => history.push(`/events/${initialValues.id}`) : () => history.push('events') }
-                                                >
-                                                            Cancel
-                                                </Button>
-                                          </Form>
-                                    </Form>
-                              </Segment>
-                        </Grid.Column>
-                  </Grid>
-                  
-            );
-      }
+  render() {
+    const {
+      history,
+      initialValues,
+      invalid,
+      submitting,
+      pristine
+    } = this.props;
+    return (
+      <Grid>
+        <Grid.Column width={10}>
+          <Segment>
+            <Form autoComplete="off" onSubmit={this.handleFormSubmit}>
+              <Header sub color="green" content="Event Details" />
+              <Form
+                onSubmit={this.props.handleSubmit(this.onFormSubmit)}
+                autoComplete="off"
+              >
+                <Field
+                  name="title"
+                  component={TextInput}
+                  placeholder="Event Name"
+                />
+
+                <Field
+                  name="category"
+                  component={SelectInput}
+                  options={category}
+                  placeholder="Event Category"
+                />
+
+                <Field
+                  name="description"
+                  component={TextArea}
+                  rows={3}
+                  placeholder="Event Decription"
+                />
+
+                <Header sub color="green" content="Event Location Details" />
+
+                <Field
+                  name="city"
+                  component={PlaceInput}
+                  options={{ type: ["(cities)"] }}
+                  onSelect={this.handleCitySelect}
+                  placeholder="Event City"
+                />
+
+                <Field
+                  name="venue"
+                  component={PlaceInput}
+                  options={{
+                    location: new google.maps.LatLng(this.state.cityLatLng),
+                    radius: 1000,
+                    types: ["establishment"]
+                  }}
+                  onSelect={this.handleVenueSelect}
+                  placeholder="Event Venue"
+                />
+
+                <Field
+                  name="date"
+                  component={DateInput}
+                  dateFormat="dd LLL yyyy h:mm a"
+                  showTimeSelect
+                  timeFormat="HH:mm"
+                  placeholder="Event Date"
+                />
+
+                <Button
+                  disabled={invalid || submitting || pristine}
+                  positive
+                  type="submit"
+                >
+                  Submit
+                </Button>
+
+                <Button
+                  type="button"
+                  onClick={
+                    initialValues.id
+                      ? () => history.push(`/events/${initialValues.id}`)
+                      : () => history.push("events")
+                  }
+                >
+                  Cancel
+                </Button>
+              </Form>
+            </Form>
+          </Segment>
+        </Grid.Column>
+      </Grid>
+    );
+  }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(reduxForm({form: 'eventForm', validate})(EventForm));
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(reduxForm({ form: "eventForm", validate })(EventForm));
--- src/features/event/eventActions.js
+++ src/features/event/eventActions.js
@@ -1,70 +1,71 @@
-import { CREATE_EVENT, UPDATE_EVENT, DELETE_EVENT, FETCH_EVENTS } from "./eventConstants";
-import { asyncActionStart, asyncActionFinish, asyncActionError } from "../async/asyncActions";
+import { UPDATE_EVENT, DELETE_EVENT, FETCH_EVENTS } from "./eventConstants";
+import {
+  asyncActionStart,
+  asyncActionFinish,
+  asyncActionError
+} from "../async/asyncActions";
 import { fetchSampleData } from "../../app/data/mockApi";
 import { toastr } from "react-redux-toastr";
 import { createNewEvent } from "../../app/common/util/helper";
-import { create } from "istanbul-reports";
 
-export const createEvent = (event) => {
-    return async (dispatch, getState, {getFirestore, getFirebase}) => {
-        const firestore = getFirestore();
-        const firebase = getFirebase();
-        const user = firebase.auth().currentUser;
-        const photoURL = getState().firebase.profile.photoURL;
-        const newEvent = createNewEvent(user, photoURL, event)
-        try {
-            let createdEvent = await firestore.add('events', newEvent);
-            await firestore.set(`event_attendee/${createdEvent.id}_${user.uid}`, {
-                eventId: createdEvent.id,
-                userUid: user.uid,
-                eventDate: event.date,
-                host: true
-            })
-            toastr.success('Success', 'Event has been created');
-            return createdEvent;
-        }
-        catch (error) {
-            toastr.error('Oops', 'Something went wrong');
-        }
-    };
-};
-
-export const updateEvent = (event) => {
-    return async dispatch => {
-        try {
-            dispatch({
-                type: UPDATE_EVENT,
-                payload: {
-                    event
-                }
-            })
-            toastr.success('Success', 'Event has been updated')
-        }
-        catch (error) {
-            toastr.error('Oops', 'Something went wrong')
-        }
-    };
-};
-
-export const deleteEvent = (eventId) => {
-    return {
-        type: DELETE_EVENT,
-        payload: {
-            eventId
-        }
+export const createEvent = event => {
+  return async (dispatch, getState, { getFirestore, getFirebase }) => {
+    const firestore = getFirestore();
+    const firebase = getFirebase();
+    const user = firebase.auth().currentUser;
+    const photoURL = getState().firebase.profile.photoURL;
+    const newEvent = createNewEvent(user, photoURL, event);
+    try {
+      let createdEvent = await firestore.add("events", newEvent);
+      await firestore.set(`event_attendee/${createdEvent.id}_${user.uid}`, {
+        eventId: createdEvent.id,
+        userUid: user.uid,
+        eventDate: event.date,
+        host: true
+      });
+      toastr.success("Success", "Event has been created");
+      return createdEvent;
+    } catch (error) {
+      toastr.error("Oops", "Something went wrong");
     }
-} 
+  };
+};
+
+export const updateEvent = event => {
+  return async dispatch => {
+    try {
+      dispatch({
+        type: UPDATE_EVENT,
+        payload: {
+          event
+        }
+      });
+      toastr.success("Success", "Event has been updated");
+    } catch (error) {
+      toastr.error("Oops", "Something went wrong");
+    }
+  };
+};
+
+export const deleteEvent = eventId => {
+  return {
+    type: DELETE_EVENT,
+    payload: {
+      eventId
+    }
+  };
+};
 
 export const loadEvents = () => {
-    return async dispatch => {
-        try {
-            dispatch(asyncActionStart());
-            let events = await fetchSampleData();
-            dispatch({type: FETCH_EVENTS, payload: {events}})
-            dispatch(asyncActionFinish())
-        } catch (error) {
-            console.log(error);
-            dispatch(asyncActionError())
-        }
+  return async dispatch => {
+    try {
+      dispatch(asyncActionStart());
+      let events = await fetchSampleData();
+      dispatch({ type: FETCH_EVENTS, payload: { events } });
+      dispatch(asyncActionFinish());
+    } catch (error) {
+      console.log(error);
+      dispatch(asyncActionError());
     }
-}
+  };
+};
