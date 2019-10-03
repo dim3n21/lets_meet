a768494e1d045e53ae768396c573e366fc2b5643
Dmitry Andrianov <dima.andrianov@gmail.com>
2019-10-03 16:35:35 -0700

    Update the mapState function.

--- src/features/event/EventDetailed/EventDetailedPage.jsx
+++ src/features/event/EventDetailed/EventDetailedPage.jsx
@@ -7,43 +7,46 @@ import EventDetailedInfo from "./EventDetailedInfo";
 import EventDetailedChat from "./EventDetailedChat";
 import EventDetailedSidebar from "./EventDetailedSidebar";
 import { withFirestore } from "react-redux-firebase";
+import { toastr } from "react-redux-toastr";
 
 const mapStateToProps = (state, ownProps) => {
-  const eventId = ownProps.match.params.id;
+      const eventId = ownProps.match.params.id;
 
-  let event = {};
+      let event = {};
 
-  if (eventId && state.events.length > 0) {
-    event = state.events.filter(event => event.id === eventId)[0];
-  }
+      if (state.firestore.ordered.events && state.firestore.ordered.events.length > 0) {
+            event = state.firestore.ordered.events.filter(event => event.id === eventId)[0];
+      }
 
-  return {
-    event
-  };
+      return {
+            event
+      };
 };
 
 class EventDetailedPage extends Component {
-
       async componentDidMount() {
-            const {firestore, match} = this.props;
+            const { firestore, match, history } = this.props;
             let event = await firestore.get(`events/${match.params.id}`);
-            console.log(event);
+            if (!event.exists) {
+                  history.push("/events");
+                  toastr.error(`Sorry`, `Event not found`);
+            }
       }
-  render() {
-        const {event} = this.props;
-    return (
-      <Grid>
-        <Grid.Column width={10}>
-          <EventDetailedHeader event={event} />
-          <EventDetailedInfo event={event} />
-          <EventDetailedChat />
-        </Grid.Column>
-        <Grid.Column width={6}>
-          <EventDetailedSidebar attendees={event.attendees} />
-        </Grid.Column>
-      </Grid>
-    );
-  }
+      render() {
+            const { event } = this.props;
+            return (
+                  <Grid>
+                        <Grid.Column width={10}>
+                              <EventDetailedHeader event={event} />
+                              <EventDetailedInfo event={event} />
+                              <EventDetailedChat />
+                        </Grid.Column>
+                        <Grid.Column width={6}>
+                              <EventDetailedSidebar attendees={event.attendees} />
+                        </Grid.Column>
+                  </Grid>
+            );
+      }
 }
 
 export default withFirestore(connect(mapStateToProps)(EventDetailedPage));
